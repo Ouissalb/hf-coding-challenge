d58538c46a59b6323ff384b0d68f5687fb92ea29
Ouissal Benameur <Ouissalb@users.noreply.github.com>
2017-12-23 12:51:15 +0000

    first commit

---
+++ src/main/java/hf/codingchallenge/ShopsListApplication.java
@@ -0,0 +1,12 @@
+package hf.codingchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopsListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopsListApplication.class, args);
+	}
+}
